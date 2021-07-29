671f0af0fe20d8a9ccb86214cc376266a6751f68
Ieshathebest <87780201+Ieshathebest@users.noreply.github.com>
2021-07-29 15:13:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,33 @@
 
+module.exports = {
+  Admins: ["277060454549815296", "391944863974948866"], //Admins of the bot
+  ExpressServer: true,//If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "r!", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/XTbSNv8SE7", //Support Server Link
+  Token: process.env.Token || "ODA3NjE5OTA0OTExMDQ4NzA0.YB6opg.-47MLUQJkxP9J-23akYT1y_5qqk", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "807619904911048704", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "21ckOzrwjC6GJq4yiImz3u6nzAhygsRt", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  CallbackURL: "https://discord.com/oauth2/authorize?client_id=807619904911048704&permissions=2205280576&redirect_uri=https%3A%2F%2Fdsc.gg%2Frytro&scope=bot", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "rytrosecret", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+
+  //Lavalink
+  Lavalink: {
+    id: "Main",
+    host: "node-1.adkynet.net",
+    port: 1111,
+    pass: "youshallnotpass",
+},
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "3b62f692d1f945d784956b066da2285c", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "51f2b58df2044c7dbcf66561b38b5de8", //Spotify Client Secret
+  },
+};
